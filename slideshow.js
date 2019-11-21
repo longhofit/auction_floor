9eadce61d2e0e6905f80dcff0ade3e50cb98ba74
Le Long Ho <lelongho998@gmail.com>
2019-11-21 20:23:53 +0700

    no message

--- slideshow.js
+++ slideshow.js
@@ -59,3 +59,15 @@ var x = setInterval(function () {
     }
 }, 1000);
 
+function abcf()
+{
+    var checkbox=document.getElementById("change-pass");
+    var disp=document.getElementById("test");
+    if(checkbox.checked==true)
+    {
+        disp.style.display="block";
+    }
+    else {
+        disp.style.display = "none";
+      }
+}
