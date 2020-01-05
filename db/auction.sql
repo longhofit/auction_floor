35adf64f42060984331367a916660a548fdb0cac
Le Long Ho <lelongho998@gmail.com>
2020-01-05 20:43:42 +0700

    edit db

--- db/auction.sql
+++ db/auction.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `online_aucdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `online_aucdb`;
 -- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
 --
 -- Host: localhost    Database: online_aucdb
@@ -446,4 +448,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-01-05  9:19:58
+-- Dump completed on 2020-01-05 20:42:35
